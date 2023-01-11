d148590df9a7242d772a6292d33abf0344239bfc
tanwar-abhi <abhishektanwar479@gmail.com>
2023-01-12 02:36:43 +0530

    basic UI layout

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+
+# Run the web application (flask)
+app = Flask(__name__)
+
+# Run the rout of the web page (python decorator)
+@app.route("/", methods = ["GET", "POST"])
+def index():
+
+    if request.method == "GET":
+        return render_template("index.html")
+
+    if request.method == "POST":
+        return render_template("dataCheck.html", FlatNo=request.form.get("FlatNo"))
