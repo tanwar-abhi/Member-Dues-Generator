76291b4be401ad636234d8495bf625afc2975b95
tanwar-abhi <abhishektanwar479@gmail.com>
2023-01-12 02:43:49 +0530

    requirements added, minor fixes

--- Functions.py
+++ Functions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec 24 15:15:41 2021
+Created on Fri Dec 24 15:15:41 2020
 
 @author: abhishek
 """
--- mainScript.py
+++ mainScript.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec 12 04:16:00 2021
+Created on Sun Dec 12 04:16:00 2020
 
 @author: abhishek
 """
